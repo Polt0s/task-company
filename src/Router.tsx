36f5b6514bdefa28db8a2e4502cb89b6ee9cf80d
polt0s <poltos.pro@gmail.com>
2021-10-11 12:40:56 +0300

    fixed naming of page components

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 import useAppSelector from './Hooks/useAppSelector';
-import About from './Pages/About/About';
-import Login from './Pages/Login/Login';
-import Main from './Pages/Main/Main';
+import About from './Pages/About/AboutPage';
+import Login from './Pages/Login/LoginPage';
+import Main from './Pages/Main/MainPage';
 
 export enum RouteNames {
   LOGIN = '/login',
